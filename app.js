70bd1481091ba334dd23e70739883dbb449ad758
Abdul Alim Rakib <abdulalimrakib04@gmail.com>
2024-02-14 17:31:33 +0600

    I remove cors

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require("./config/database");
 
 const express = require("express");
-const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const authRouter = require("./routes/auth.route");
@@ -12,12 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
